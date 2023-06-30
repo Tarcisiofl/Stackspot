10b7f0621700555054978cc34cd7a8c473943e89
Tarcisio Lima <p0651239@portoseguro.com.br>
2023-06-30 16:29:16 -0300

    Created plugin template

---
+++ porto-robot-plugin/templates/robot/Ast.kt
@@ -0,0 +1,8 @@
+package br.com.portoseguro.authfy.camera.presentation.entry.robot
+
+import br.com.portoseguro.portobot.koin.KoinBridgeTest
+import br.com.portoseguro.portobot.robot.contract.RobotAssert
+
+internal class Ast : RobotAssert, KoinBridgeTest {
+
+}
---
+++ porto-robot-plugin/templates/robot/Def.kt
@@ -0,0 +1,7 @@
+package br.com.portoseguro.authfy.camera.presentation.entry.robot
+
+import br.com.portoseguro.portobot.robot.contract.RobotDefinition
+
+internal class Def : RobotDefinition {
+
+}
